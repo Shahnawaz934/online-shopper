c3aafb56b8b6f7faec2e91df3133e82eb587026b
Shahnawaz alam <alam.shahnawaz666@gmail.com>
2024-02-14 22:26:07 +0530

    changes added

--- js/signin.js
+++ js/signin.js
@@ -3,34 +3,6 @@ document.getElementById("continueBtn").addEventListener("click",ContinueClick);
 document.querySelector("form").addEventListener("submit",FormSubmit);
 
 
-
-function ContinueClick(){
-    event.preventDefault();
-    document.querySelector("form").style.width = "500px";
-    fetch("https://shopper-backend-fdhf.onrender.com/users")
-    .then(response=>{
-        return response.json()
-    })
-    .then((users)=>{
-        users.map(user=>{
-            // alert("hi")
-    var txtEmail = document.getElementById("txtEmail");
-
-    if(txtEmail.value==user.Email || txtEmail.value==user.Mobile){
-         document.getElementById("passwordContainer").className = "d-block";
-        document.getElementById("error").style.display = "none";
-         document.getElementById("continueContainer").style.display = "none"
-        // document.querySelector("form").className = "border p-3 rounded-4 w-25 bg-secondary text-bg-dark h-75 m-4";
-        document.querySelector("form").style.width = "300px"
-    }
-    else{
-        document.querySelector("form").style.width = "300px"
-        document.getElementById("error").innerHTML = "Invalid Email or Mobile".fontcolor('red');
-    }
-        })
-    })
-}
-
 function FormSubmit(){
     event.preventDefault();
     fetch("https://shopper-backend-fdhf.onrender.com/users")
@@ -71,4 +43,31 @@ function FormSubmit(){
             }
         })
     })    
+}
+
+function ContinueClick(){
+    event.preventDefault();
+    document.querySelector("form").style.width = "500px";
+    fetch("https://shopper-backend-fdhf.onrender.com/users")
+    .then(response=>{
+        return response.json()
+    })
+    .then((users)=>{
+        users.map(user=>{
+            // alert("hi")
+    var txtEmail = document.getElementById("txtEmail");
+
+    if(txtEmail.value==user.Email || txtEmail.value==user.Mobile){
+         document.getElementById("passwordContainer").className = "d-block";
+        document.getElementById("error").style.display = "none";
+         document.getElementById("continueContainer").style.display = "none"
+        // document.querySelector("form").className = "border p-3 rounded-4 w-25 bg-secondary text-bg-dark h-75 m-4";
+        document.querySelector("form").style.width = "300px"
+    }
+    else{
+        document.querySelector("form").style.width = "300px"
+        document.getElementById("error").innerHTML = "Invalid Email or Mobile".fontcolor('red');
+    }
+        })
+    })
 }
